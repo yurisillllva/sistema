58a691e5219addbd8bdba3fd3349540cf2017c62
yuriMartins <ysm281@gmail.com>
2021-10-17 15:02:29 -0300

    add alertas

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,6 +1,7 @@
 
 import { useState, createContext, useEffect } from 'react';
 import firebase from '../services/firebaseConnection';
+import { toast } from 'react-toastify';
 
 export const AuthContext = createContext({});
 
@@ -47,10 +48,12 @@ function AuthProvider({ children }){
       setUser(data);
       storageUser(data);
       setLoading(false);
+      toast.success('Bem vindo de volta!');
 
     })
     .catch((error)=>{
       console.log(error);
+      toast.error('Ops algo deu errado!'); 
       setLoadingAuth(false);
     })
   }
@@ -80,12 +83,15 @@ function AuthProvider({ children }){
         setUser(data);
         storageUser(data);
         setLoading(false);
+        toast.success('Bem Vindo a plataforma!');
 
       })
     })
     .catch((error) =>{
       console.log(error);
+      toast.error('Ops algo deu errado!');
       setLoadingAuth(false);
+      
     })
   }
 
